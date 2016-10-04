8612fd94c38474d0dcf7004d1dfa13f40c558920
haisapan <haisapan@hotmail.com>
2016-10-04 23:00:55 +0800

    some changes

--- src/components/CoreDataTable/CoreDataTable.js
+++ src/components/CoreDataTable/CoreDataTable.js
@@ -9,82 +9,12 @@ import CoreDataTable_SearchBar from './CoreDataTable_SearchBar/CoreDataTable_Sea
 import CoreDataTable_ToolBar from './CoreDataTable_ToolBar/CoreDataTable_ToolBar';
 import CoreDataTable_MainTable from './CoreDataTable_MainTable/CoreDataTable_MainTable';
 
-// const columns = [{
-//   title: '姓名',
-//   dataIndex: 'name',
-//   key: 'name',
-//   width: '40%',
-// }, {
-//   title: '年龄',
-//   dataIndex: 'age',
-//   key: 'age',
-//   width: '30%',
-// }, {
-//   title: '住址',
-//   dataIndex: 'address',
-//   key: 'address',
-//   width: '30%',
-// }];
-
-// const data = [{
-//   key: 1,
-//   name: 'a',
-//   age: 32,
-//   address: '我是a',
-//   children: [{
-//     key: 11,
-//     name: 'aa',
-//     age: 33,
-//     address: '我是aa',
-//   }, {
-//     key: 12,
-//     name: 'ab',
-//     age: 33,
-//     address: '我是ab',
-//     children: [{
-//       key: 121,
-//       name: 'aba',
-//       age: 33,
-//       address: '我是aba',
-//     }],
-//   }, {
-//     key: 13,
-//     name: 'ac',
-//     age: 33,
-//     address: '我是ac',
-//     children: [{
-//       key: 131,
-//       name: 'aca',
-//       age: 33,
-//       address: '我是aca',
-//       children: [{
-//         key: 1311,
-//         name: 'acaa',
-//         age: 33,
-//         address: '我是acaa',
-//       }, {
-//         key: 1312,
-//         name: 'acab',
-//         age: 33,
-//         address: '我是acab',
-//       }],
-//     }],
-//   }],
-// }, {
-//   key: 2,
-//   name: 'b',
-//   age: 32,
-//   address: '我是b',
-// }];
-
-
-
 class CoreDataTable extends Component {
 
     constructor(props) {
         super(props);
         this.filterTable = this.filterTable.bind(this);
-        this.state={
+        this.state = {
             pagination: {},
         };
     };
@@ -135,12 +65,12 @@ onSelectChange(selectedRowKeys) {
 };
 
 
-filterTable(pagination={}, filters, sorter={}){
-        this.fetch({
-            results: pagination.pageSize,
-            page: pagination.current,
-            sortField: sorter.field,
-            sortOrder: sorter.order,
+filterTable(pagination = {}, filters, sorter = {}){
+    this.fetch({
+        results: pagination.pageSize,
+        page: pagination.current,
+        sortField: sorter.field,
+        sortOrder: sorter.order,
             ...filters,
     });
 };
